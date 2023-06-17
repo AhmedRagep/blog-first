86135850f89166021a9269bf7c21003d04e599b0
Ahmed-Altorky-Android <viphaker15@gmail.com>
2023-06-17 18:21:01 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 function App() {
   return (
     <>
-      <h1 className="title">Hello World</h1>
+      <h1 className="title">Hello World This New Version</h1>
       <h2>This My React App</h2>
     </>
   );
