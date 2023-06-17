cecd32ff72ee2270f58df3b8f90e1d1ee20f8931
Ahmed-Altorky-Android <viphaker15@gmail.com>
2023-06-17 12:09:18 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="title">Hello World</h1>
+      <h2>This My React App</h2>
+    </>
+  );
+}
+
+export default App;
